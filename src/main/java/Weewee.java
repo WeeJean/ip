76231ef73689f855ee350b689ab3ed1b0639de59
Wee Jean <E1354206@u.nus.edu>
2025-08-20 20:06:07 +0800

    Renamed, greeting and exit added

---
+++ src/main/java/Weewee.java
@@ -0,0 +1,7 @@
+public class Weewee {
+    public static void main(String[] args) {
+        String greet = "Hello! I'm Weewee\n" + "What can I do for you?";
+        String bye = "Bye. Hope to see you again soon!";
+        System.out.println(greet + "\n" + bye);
+    }
+}
