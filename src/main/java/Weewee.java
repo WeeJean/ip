40ba7e55b784a9637016fd157153418856eeed7d
Wee Jean <E1354206@u.nus.edu>
2025-08-20 21:11:26 +0800

    Add, List function implemented

--- src/main/java/Weewee.java
+++ src/main/java/Weewee.java
@@ -1,9 +1,11 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Weewee {
     public static void main(String[] args) {
         String greet = "Hello! I'm Weewee\n" + "What can I do for you?";
         String bye = "Bye. Hope to see you again soon!";
+        ArrayList<String> toDo = new ArrayList<>(100);
 
         System.out.println(greet + "\n");
 
@@ -11,10 +13,20 @@ public class Weewee {
         String input = sc.nextLine();
 
         while(!input.equals("bye")) {
-            System.out.println(input+ "\n");
-            input = sc.nextLine();
-        }
 
+            //if input in command: list
+            if (input.equals("list")) {
+                for (int i = 0; i < toDo.size(); i++) {
+                    System.out.printf("%d. %s\n", i + 1, toDo.get(i));
+                }
+                input = sc.nextLine();
+            } else {
+                //if input is task
+                System.out.println("added: " + input + "\n");
+                toDo.add(input);
+                input = sc.nextLine();
+            }
+        }
         System.out.println(bye);
     }
 }
