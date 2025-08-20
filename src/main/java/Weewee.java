e5eee09124b47dc2018d895ba5fdfec42cd6ac62
Wee Jean <E1354206@u.nus.edu>
2025-08-20 20:35:06 +0800

    echo commands and exit when command bye

--- src/main/java/Weewee.java
+++ src/main/java/Weewee.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
+
 public class Weewee {
     public static void main(String[] args) {
         String greet = "Hello! I'm Weewee\n" + "What can I do for you?";
         String bye = "Bye. Hope to see you again soon!";
-        System.out.println(greet + "\n" + bye);
+
+        System.out.println(greet + "\n");
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        while(!input.equals("bye")) {
+            System.out.println(input+ "\n");
+            input = sc.nextLine();
+        }
+
+        System.out.println(bye);
     }
 }
