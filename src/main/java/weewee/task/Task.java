7440e39f7b49825dc149f4681a9ec0be8edf21f2
Wee Jean <E1354206@u.nus.edu>
2025-08-28 12:59:35 +0800

    Format code

--- src/main/java/weewee/task/Task.java
+++ src/main/java/weewee/task/Task.java
@@ -31,6 +31,7 @@ public class Task {
 
     @Override
     public String toString() {
-        return getIsdone() + " " + taskName;
+        String s = getIsdone() + " " + taskName;
+        return s;
     }
 }
